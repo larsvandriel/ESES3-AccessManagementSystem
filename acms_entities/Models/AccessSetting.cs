8d1975b9b975126580a444d48efaad48e8160452
larsvandriel <lars.van.driel@hotmail.com>
2022-01-17 00:19:51 +0100

    Added Helper classes

--- acms_entities/Models/AccessSetting.cs
+++ acms_entities/Models/AccessSetting.cs
@@ -2,7 +2,9 @@
 {
     public class AccessSetting
     {
+        public Guid ElectricalLockId { get; set; }
         public ElectronicLock Lock { get; set; }
+        public Guid AccessorId { get; set; }
         public Accessor Accessor { get; set; }
         public bool AllowedToEnter { get; set; }
     }
